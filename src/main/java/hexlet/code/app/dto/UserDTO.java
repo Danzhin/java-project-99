4cf34c64a12f09015bf817843946739d481fdf19
five <dronzhin123@gmail.com>
2025-06-30 20:24:06 +0300

    update

--- src/main/java/hexlet/code/app/dto/UserDTO.java
+++ src/main/java/hexlet/code/app/dto/UserDTO.java
@@ -3,16 +3,12 @@ package hexlet.code.app.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
 @Getter
 @Setter
 public class UserDTO {
     private Long id;
     private String firstName;
     private String lastName;
-    private String email;
+    private String username;
     private String password;
-    private LocalDate createdAt;
-    private LocalDate updatedAt;
 }
