1e9cb47f45add2398e812bd446d4b80351a4fc67
five <dronzhin123@gmail.com>
2025-06-30 13:27:14 +0300

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	checkstyle
+	//checkstyle
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("io.freefair.lombok") version "8.6"
